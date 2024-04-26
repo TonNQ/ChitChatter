c8cfb0052898ec5a19199fb44ec7f67a9aaab92c
TraMyNguyen39 <tram8319@gmail.com>
2024-04-26 16:41:55 +0700

    add createAccount, login, updateAccount, updateToken function

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,112 @@ const { setGlobalOptions } = require('firebase-functions/v2')
 admin.initializeApp({ credential: admin.credential.applicationDefault() })
 setGlobalOptions({ maxInstances: 10 })
 
+exports.addAccount = onRequest(async (req, res) => {
+  const account = req.body
+  const collectionRef = getFirestore().collection('accounts')
+
+  await collectionRef
+    .doc(account.username)
+    .get() // check if account existed
+    .then((result) => {
+      if (!result.exists) {
+      // Add account if not existed
+        collectionRef
+          .doc(account.username).set(account)
+        updateToken(account.username, account.token)
+        res.json({
+          success: true,
+          targetAccount: account,
+          error: null
+        })
+      } else {
+        res.json({
+          success: false,
+          targetAccount: null,
+          error: 'Username đã tồn tại!'
+        })
+      }
+    })
+    .catch(() => {
+      res.json({
+        success: false,
+        targetAccount: null,
+        error: 'Có lỗi xảy ra trong khi đăng ký tài khoản của bạn!'
+      })
+    })
+})
+
+exports.updateAccount = onRequest(async (req, res) => {
+  const account = req.body
+  const collectionRef = getFirestore().collection('accounts')
+
+  await collectionRef
+    .doc(account.username)
+    .update({
+      displayName: account.displayName,
+      email: account.email,
+      age: account.age
+    })
+    .then((result) => {
+      collectionRef.doc(account.username).set(account)
+      updateToken(account.username, account.token)
+      res.json({
+        success: true,
+        error: null
+      })
+    })
+    .catch(() => {
+      res.json({
+        success: false,
+        error: `Tài khoản với username "${account.username}" không tồn tại!`
+      })
+    })
+})
+
+/**
+ * @param {string} username id needed to get account
+ * @param {string} token token to update
+ */
+
+async function updateToken (username, token) {
+  const timestamp = Math.floor(Date.now() / 1000)
+  const tokenObject = {
+    token: token,
+    timestamp: timestamp
+  }
+  await getFirestore()
+    .collection('tokens')
+    .doc(username)
+    .set(tokenObject)
+}
+
+exports.login = onRequest(async (req, res) => {
+  const account = req.body
+  const docRef = getFirestore()
+    .collection('accounts')
+    .doc(account.username)
+
+  await docRef
+    .get()
+    .then((result) => {
+      if (result.exists) {
+        const targetAccount = result.data()
+        if (targetAccount.password === account.password) {
+          updateToken(account.username, account.token)
+          targetAccount.token = account.token
+          res.json(targetAccount)
+        } else {
+          res.json(null)
+        }
+      } else {
+        res.json(null)
+      }
+    })
+    .catch(() => {
+      res.json(null)
+    })
+})
+
 exports.addMessage = onRequest(async (req, res) => {
   const msg = req.query.text
   const result = await getFirestore().collection('messages').add({ message: msg })
@@ -29,7 +135,7 @@ exports.sendMessage = onRequest(async (req, res) => {
  * Send notification to user
  * @param {Object} body
  */
-async function sendNotification(body) {
+async function sendNotification (body) {
   const token = body.token
   const message = {
     token: token,
