448e7e9c6001cd5b1a6477b930a4afde6952dea1
thanhdangg <102210310@sv1.dut.udn.vn>
2024-05-01 00:35:26 +0700

    hmmm

--- functions/index.js
+++ functions/index.js
@@ -25,25 +25,68 @@ exports.sendMessage = onRequest(async (req, res) => {
   })
 })
 
+exports.getChat = onRequest(async (req, res) => {
+  const sender = req.query.sender;
+  const receiver = req.query.receiver;
+  const messageRef = getFirestore().collection("messages");
+  const snapshot = await messageRef
+      .where(
+          Filter.or(
+              Filter.where("sender", "==", sender),
+              Filter.where("sender", "==", receiver),
+          ),
+      )
+      .where(
+          Filter.or(
+              Filter.where("receiver", "==", sender),
+              Filter.where("receiver", "==", receiver),
+          ),
+      )
+      .orderBy("timestamp", "desc")
+      .limit(100)
+      .get();
+  const messages = [];
+  snapshot.forEach((doc) => {
+    messages.push(doc.data());
+  });
+  res.json(messages);
+});
+
+exports.sendMessage = onRequest(async (req, res) => {
+  const message = req.body;
+  await getFirestore()
+      .collection("messages")
+      .add(message);
+  await sendNotification(message);
+  res.json({
+    success: true,
+    error: null,
+  });
+});
+
+
 /**
  * Send notification to user
  * @param {Object} body
  */
 async function sendNotification(body) {
   const token = body.token
+  const photoUrl = body.data.photoUrl;
+  const photoMimeType = body.data.photoMimeType;
   const message = {
     token: token,
     data: {
       text: body.data.text,
-      photoUri: body.data.photoUri,
-      photoMimeType: body.data.photoMimeType,
+      photoUrl: photoUrl == null ? 'null' : photoUrl,
+      photoMimeType: photoMimeType  == null ? 'null' : photoMimeType,
       sender: body.data.sender,
       receiver: body.data.receiver
     },
     notification: {
       title: body.notification.title,
-      body: body.notification.body
-    }
+      body: body.notification.body == null ? 'null' : body.notification.body,
+    },
+    token: token,
   }
   admin
     .messaging()
