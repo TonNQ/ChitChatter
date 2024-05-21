719278c4fa80f369898001663ca1bca599c091a1
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-05-21 22:26:37 +0700

    send message api

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/default */
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-invalid-this */
 const { formatTimestamp, parseDateString, displayTime } = require('./utils/utils')
@@ -5,16 +6,34 @@ const { getAllAccounts } = require('./modules/accounts')
 
 // Firebase
 const { onRequest } = require('firebase-functions/v2/https')
-// const logger = require('firebase-functions/logger')
-// const { onDocumentCreated } = require('firebase-functions/v2/firestore')
 
 const admin = require('firebase-admin')
 const { getAuth } = require('firebase-admin/auth')
-const { getFirestore } = require('firebase-admin/firestore')
+const { getFirestore, Filter } = require('firebase-admin/firestore')
 const { setGlobalOptions } = require('firebase-functions/v2')
 
 admin.initializeApp({ credential: admin.credential.applicationDefault() })
 setGlobalOptions({ maxInstances: 10 })
+
+const firestore = admin.firestore()
+firestore.settings({ ignoreUndefinedProperties: true })
+
+const { getDatabase, ref, set } = require('firebase/database')
+const { initializeApp } = require('firebase/app')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCS8iWccA0Vh0IWgirTxRzWYR8f3XDOCWo',
+  authDomain: 'chitchatter-b97bf.firebaseapp.com',
+  databaseURL: 'https://chitchatter-b97bf-default-rtdb.firebaseio.com',
+  projectId: 'chitchatter-b97bf',
+  storageBucket: 'chitchatter-b97bf.appspot.com',
+  messagingSenderId: '32426056104',
+  appId: '1:32426056104:web:58f3f2630f4a92b113659d',
+  measurementId: 'G-XFNPG8PBTC'
+}
+
+const app = initializeApp(firebaseConfig)
+const firebaseDb = getDatabase(app)
 
 exports.createAccountInFirestore = onRequest(async (req, res) => {
   const account = req.body
@@ -127,46 +146,159 @@ exports.addMessage = onRequest(async (req, res) => {
   res.json({ result: `MessageId: ${result.id}` })
 })
 
+const sendMessageToRealtimeDb = async (message) => {
+  try {
+    // Extract username from sender and receiver emails
+    const senderUsername = message.sender.split('@')[0]
+    const receiverUsername = message.receiver.split('@')[0]
+
+    // Sanitize extracted usernames for Firebase paths
+    const sanitizedSender = senderUsername
+    const sanitizedReceiver = receiverUsername
+
+    // Construct the path for the message
+    const messagePath = `messages/${sanitizedSender}/${sanitizedReceiver}`
+
+    // Reference to the database path
+    const dbRef = ref(firebaseDb, messagePath)
+
+    // Write the message to the Realtime Database
+    await set(dbRef, message)
+    console.log('Data written successfully')
+  } catch (error) {
+    console.error('Error writing data: ', error)
+  }
+}
+
 exports.sendMessage = onRequest(async (req, res) => {
-  const message = req.body
-  await getFirestore().collection('messages').add(message)
-  await sendNotification(message)
-  res.json({
-    success: true,
-    error: null
-  })
+  let isSent = false
+  try {
+    const data = req.body
+    const isTokenValid = await checkToken(data.sender, data.token)
+    if (isTokenValid) {
+      const message = {
+        content: data.content || null,
+        sender: data.sender || null,
+        receiver: data.receiver || null,
+        photoUrl: data.photoUrl || null,
+        photoMimeType: data.photoMimeType || null,
+        createdAt: new Date(),
+        status: 1
+      }
+
+      // Kiểm tra các trường bắt buộc
+      if (!message.content || !message.sender || !message.receiver) {
+        res.status(400).json({ success: false, data: null, error: 'Thiếu các trường bắt buộc' })
+        return
+      }
+
+      console.log('Constructed message:', message)
+
+      // Lưu tin nhắn vào Firestore
+      await getFirestore().collection('messages').add(message)
+      message.createdAt = formatTimestamp(new Date(message.createdAt))
+
+      // Trả về mã trạng thái 200 trước khi gửi thông báo
+      res.status(200).json({ success: true, data: message, error: null })
+      isSent = true
+
+      // Lấy các token FCM của người nhận
+      const receiverDoc = await getFirestore().collection('accounts').doc(message.receiver).get()
+      if (!receiverDoc.exists) {
+        console.error('Người nhận không tồn tại')
+        res.status(400).json({ success: false, data: null, error: 'Người nhận không tồn tại' })
+        return
+      }
+
+      const receiverData = receiverDoc.data()
+      const fcmTokens = receiverData.tokens || []
+
+      const isOnline = fcmTokens.some((token) => {
+        return typeof token.token === 'string' && token.token.trim() !== '' && token.isOnline
+      })
+      if (isOnline) {
+        sendMessageToRealtimeDb(message)
+      } else {
+        // Loại bỏ các token không hợp lệ
+        const validTokens = fcmTokens
+          .filter((token) => {
+            return typeof token.token === 'string' && token.token.trim() !== '' && !token.isOnline
+          })
+          .map((token) => token.token)
+
+        console.log(validTokens)
+        if (validTokens.length > 0) {
+          sendNotification(validTokens, data)
+        }
+      }
+    } else {
+      console.log('error token')
+      res.status(400).json({ success: false, data: null, error: 'Token không hợp lệ' })
+    }
+  } catch (error) {
+    console.error('Error:', error)
+    if (!isSent) {
+      res.status(500).json({ success: false, data: null, error: 'Lỗi máy chủ nội bộ' })
+    }
+  }
 })
+
+const sendNotification = async (tokens, data) => {
+  try {
+    const message = {
+      notification: {
+        title: 'New message',
+        body: data.content
+      },
+      data: {
+        content: String(data.content),
+        sender: String(data.sender),
+        receiver: String(data.receiver),
+        photoUrl: String(data.photoUrl || ''),
+        photoMimeType: String(data.photoMimeType || ''),
+        createdAt: String(new Date().toISOString()),
+        status: String(1)
+      },
+      tokens: tokens
+    }
+
+    await admin.messaging().sendEachForMulticast(message)
+    console.log('Notification sent successfully')
+  } catch (error) {
+    console.error('Error sending multicast message:', error)
+  }
+}
 
 /**
  * Send notification to user
  * @param {Object} body
  */
-async function sendNotification(body) {
-  const token = body.token
-  const message = {
-    token: token,
-    data: {
-      text: body.data.text,
-      photoUri: body.data.photoUri,
-      photoMimeType: body.data.photoMimeType,
-      sender: body.data.sender,
-      receiver: body.data.receiver
-    },
-    notification: {
-      title: body.notification.title,
-      body: body.notification.body
-    }
-  }
-  admin
-    .messaging()
-    .send(message)
-    .then((response) => {
-      console.log('success', response)
-    })
-    .catch((error) => {
-      console.log('failed', error)
-    })
-}
+// async function sendNotification(body) {
+//   const token = body.token
+//   const message = {
+//     token: token,
+//     data: {
+//       text: body.data.text,
+//       photoUri: body.data.photoUri,
+//       photoMimeType: body.data.photoMimeType,
+//       sender: body.data.sender,
+//       receiver: body.data.receiver
+//     },
+//     notification: {
+//       title: body.notification.title,
+//       body: body.notification.body
+//     }
+//   }
+//   admin
+//     .messaging()
+//     .send(message)
+//     .then((response) => {
+//       console.log('success', response)
+//     })
+//     .catch((error) => {
+//       console.log('failed', error)
+//     })
+// }
 
 exports.getAllAccounts = async function () {
   const accounts = await getFirestore().collection('accounts').get()
@@ -176,6 +308,26 @@ exports.getAllAccounts = async function () {
   })
   return data
 }
+
+exports.getChat = onRequest(async (req, res) => {
+  const sender = req.query.sender
+  const receiver = req.query.receiver
+  const messageRef = getFirestore().collection('messages')
+  const snapshot = await messageRef
+    .where(Filter.or(Filter.where('sender', '==', sender), Filter.where('sender', '==', receiver)))
+    .where(Filter.or(Filter.where('receiver', '==', sender), Filter.where('receiver', '==', receiver)))
+    .orderBy('createdAt', 'desc')
+    .limit(100)
+    .get()
+  const messages = []
+  snapshot.forEach((doc) => {
+    const message = doc.data()
+    const formattedTime = formatTimestamp(message.createdAt)
+    message.formattedTime = formattedTime
+    messages.push(message)
+  })
+  res.json(messages)
+})
 
 exports.getAllLastMessages = onRequest(async (req, res) => {
   const currentAccount = req.query.email
@@ -215,3 +367,18 @@ exports.getAllLastMessages = onRequest(async (req, res) => {
     res.status(500).json({ success: false, data: null, error: 'Internal server error' })
   }
 })
+
+async function checkToken(email, token) {
+  try {
+    const accountDoc = await getFirestore().collection('accounts').doc(email).get()
+    if (accountDoc.exists) {
+      const tokens = accountDoc.data().tokens
+      const tokenObj = tokens.find((tokenObj) => tokenObj.token === token)
+      return tokenObj ? true : false
+    }
+    return false
+  } catch (error) {
+    console.error('Error in checkToken:', error)
+    return false
+  }
+}
