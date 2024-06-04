8faaec4dbbc6ef14cce5ba5cd809bd87f81020c2
TraMyNguyen39 <tram8319@gmail.com>
2024-06-04 14:52:19 +0700

    add function acceptContact

--- functions/index.js
+++ functions/index.js
@@ -306,6 +306,43 @@ async function sendContactRequest(userEmail, contactEmail, tokens) {
   }
 }
 
+exports.acceptContact = onRequest(async (req, res) => {
+  const connection = req.body
+  const sender = connection.sender
+  const receiver = connection.receiver
+  const token = connection.token
+  try {
+    const isTokenValid = await checkToken(sender, token)
+    if (!isTokenValid) {
+      res.status(401).json({ success: false, error: 'Token is invalid' })
+      return
+    }
+
+    const requestDoc = await getFirestore().collection('request-contact').doc(`${receiver}_${sender}`).get()
+    if (requestDoc.exists) {
+      await getFirestore().collection('request-contact').doc(`${receiver}_${sender}`).delete()
+      await getFirestore()
+        .collection('accounts')
+        .doc(sender)
+        .update({
+          contacts: FieldValue.arrayUnion(receiver)
+        })
+      await getFirestore()
+        .collection('accounts')
+        .doc(receiver)
+        .update({
+          contacts: FieldValue.arrayUnion(sender)
+        })
+      res.status(200).json({ success: true, error: null })
+    } else {
+      res.status(404).json({ success: false, error: 'Request not found' })
+    }
+  } catch (error) {
+    console.error('Error fetching account:', error)
+    res.status(500).json({ success: false, error: 'Internal server error' })
+  }
+})
+
 exports.addMessage = onRequest(async (req, res) => {
   const msg = req.query.text
   const result = await getFirestore().collection('messages').add({ message: msg })
